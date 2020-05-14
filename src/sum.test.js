dc4443c2b5fcffc3b8dd571f9e0167d7e3ff4ff8
William Koller <wkoller25@gmail.com>
2020-05-14 13:05:08 -0300

    add others tests

--- src/sum.test.js
+++ src/sum.test.js
@@ -1,5 +1,34 @@
 const sum = require('./sum');
 
 test('adds 1 + 2 to equal 3', () => {
-  expect(sum(1,2)).toEqual(3);
+  expect(sum(1,2)).toEqual(3)
+})
+
+test('Adding positive numbers is not zero', () => {
+  for (let a = 1; a < 10; a++) {
+    for (let b = 1; b < 10; b++) {
+      expect(a + b).not.toBe(0)
+    } 
+  } 
+})
+
+test('Object assignment', () => {
+  const data = { one: 1}
+  data['two'] = 2
+  expect(data).toEqual({ one:1, two: 2})
+})
+
+test('Two plus two', () => {
+  const value = 2 + 2
+  expect(value).toBeGreaterThan(3)
+  expect(value).toBeGreaterThanOrEqual(3.5)
+  expect(value).toBeLessThan(5)
+  expect(value).toBeLessThanOrEqual(4.5)
+  expect(value).toBe(4)
+  expect(value).toEqual(4)
+})
+
+test('Adding floating point', () => {
+  const value = 0.1 + 0.2
+  expect(value).toBeCloseTo(0.3)
 })
